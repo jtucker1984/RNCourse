875c513107e5255b715b95b05edffedfff3f7a19
Jeremy Tucker <jeremytucker76@gmail.com>
2023-08-28 19:55:16 -0400

    React training

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput , Button, ScrollView} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+const [enteredGoalText, setEnteredGoalText]= useState([]);
+const [courseGoals, setCourseGoals]= useState([]);
+
+  function goalInput(enteredText){
+    setEnteredGoalText(enteredText);
+  } 
+
+  function goalList(){
+    setCourseGoals(currentCourseGoals=>[
+      ...currentCourseGoals,
+      enteredGoalText
+    ]);
+  }
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}  >
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput}
+         placeholder='Text Here' 
+         onChangeText={goalInput}>
+
+        </TextInput>
+        <Button title="Add Goal" onPress={goalList} />
+      </View>
+
+      <ScrollView style={styles.labelContainer}>
+        {courseGoals.map((goal)=>
+        
+        <Text style={styles.goalItem} key={goal}>{goal}</Text>)}
+        
+      </ScrollView>
+
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal:16,
+    
   },
+  inputContainer: {
+    flexDirection: 'row',
+    flex:1,
+    justifyContent:'space-between',
+    alignItems:'center',
+    marginBottom: 24,
+    borderBottomWidth:1
+
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'blue',
+    width:'70%',
+    marginRight:8,
+    padding:8,
+    
+    
+  },
+  labelContainer:{
+    flex:6
+  },
+  goalItem:{
+    margin: 8,
+    padding:8,
+    borderRadius:6,
+    backgroundColor:'salmon',
+    color:'black'
+  }
 });
